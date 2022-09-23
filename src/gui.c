5964c8541a4cda8b7193762e355628f8e7e81164
Luca Morselli <luca.morselli@lnl.infn.it>
2022-09-23 15:00:19 +0200

    small ui changes

--- src/gui.c
+++ src/gui.c
@@ -33,31 +33,31 @@ void refresh_gui_monitor(caen_hv_state* s)
    char monitor_string[50];
 
    // Update VSET
-   sprintf(monitor_string,"VSET: %.2f", s->vset);
+   sprintf(monitor_string,"VSET [V]: %.2f", s->vset);
    gtk_label_set_text(GTK_LABEL(label_vset), (const gchar *) monitor_string);
 
    // Update ISET
-   sprintf(monitor_string,"ISET: %.2f", s->iset);
+   sprintf(monitor_string,"ISET [V]: %.2f", s->iset);
    gtk_label_set_text(GTK_LABEL(label_iset), (const gchar *) monitor_string);
 
    // Update VMON
-   sprintf(monitor_string,"Voltage: %.2f", s->voltage_level);
+   sprintf(monitor_string,"Voltage [V]: %.2f", s->voltage_level);
    gtk_label_set_text(GTK_LABEL(label_voltage_level), (const gchar *) monitor_string);
 
    // Update IMON
-   sprintf(monitor_string,"Current: %.2f", s->current_level);
+   sprintf(monitor_string,"Current [uA]: %.2f", s->current_level);
    gtk_label_set_text(GTK_LABEL(label_current_level), (const gchar *) monitor_string);
 
    // Update RAMPUP
-   sprintf(monitor_string,"RAMP UP: %.2f", s->ramp_up);
+   sprintf(monitor_string,"RAMP UP [V]: %.2f", s->ramp_up);
    gtk_label_set_text(GTK_LABEL(label_ramp_up), (const gchar *) monitor_string);
 
    // Update RAMPDOWN
-   sprintf(monitor_string,"RAMP DOWN: %.2f", s->ramp_down);
+   sprintf(monitor_string,"RAMP DOWN [V]: %.2f", s->ramp_down);
    gtk_label_set_text(GTK_LABEL(label_ramp_down), (const gchar *) monitor_string);
 
    // Update TRIP
-   sprintf(monitor_string,"TRIP: %.2f", s->trip);
+   sprintf(monitor_string,"TRIP [V]: %.2f", s->trip);
    gtk_label_set_text(GTK_LABEL(label_trip), (const gchar *) monitor_string);
 
    // Update switch buttons
@@ -71,8 +71,7 @@ void refresh_gui_monitor(caen_hv_state* s)
 void init_gui()
 {
    // Build GUI from GLADE XML File
-   builder = gtk_builder_new_from_file("glade/gui.glade");
-
+  builder = gtk_builder_new_from_file("glade/gui.glade");
    // Main Window and Fixed Container
    window  = GTK_WIDGET(gtk_builder_get_object(builder,"window"));
    fixed   = GTK_WIDGET(gtk_builder_get_object(builder,"fixed"));
